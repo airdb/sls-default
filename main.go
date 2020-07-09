db00a0262a3c16e4cf7f17a4901ea74bd59cb6f8
deancn <dean@airdb.com>
2020-07-10 02:09:52 +0800

    add action

--- main.go
+++ main.go
@@ -1,25 +1,21 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "github.com/tencentyun/scf-go-lib/cloudfunction"
-    "github.com/tencentyun/scf-go-lib/cloudevents/scf"
+	"context"
+	"fmt"
+
+	"github.com/tencentyun/scf-go-lib/cloudevents/scf"
+	"github.com/tencentyun/scf-go-lib/cloudfunction"
 )
 
-type DefineEvent struct {
-    // test event define
-    Key1 string `json:"key1"`
-    Key2 string `json:"key2"`
-}
-
 func hello(ctx context.Context, req scf.APIGatewayProxyRequest) (string, error) {
-    fmt.Println("req:", req)
-    // return fmt.Sprintf("Hello %s!", req.Body), nil
-    return fmt.Sprintf("Hello %s!", req.QueryString), nil
+	fmt.Println("req:", req)
+	fmt.Println("req:", req)
+	// return fmt.Sprintf("Hello %s!", req.Body), nil
+	return fmt.Sprintf("Hello %s!", req.QueryString), nil
 }
 
 func main() {
-    // Make the handler available for Remote Procedure Call by Cloud Function
-    cloudfunction.Start(hello)
+	// Make the handler available for Remote Procedure Call by Cloud Function
+	cloudfunction.Start(hello)
 }
