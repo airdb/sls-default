7b94dddff8ef5845495434e8856833570f30dc18
deancn <dean@airdb.com>
2021-04-18 00:48:26 +0800

    update

--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module github.com/airdb/scf-airdb
 go 1.16
 
 require (
-	airdb.io/airdb/sailor v0.0.5
-	github.com/gin-gonic/gin v1.6.3
+	airdb.io/airdb/sailor v0.0.8
+	github.com/gin-gonic/gin v1.7.1
 	github.com/pkg/errors v0.8.1
 	github.com/serverless-plus/tencent-serverless-go v0.1.1
 	github.com/spf13/cobra v1.1.3
-	gorm.io/gorm v1.20.0
+	gorm.io/gorm v1.21.8
 )
--- pkg/airsdk/hosted_zone.go
+++ pkg/airsdk/hosted_zone.go
@@ -1,24 +1,41 @@
 package airsdk
 
 import (
+	"encoding/json"
+	"fmt"
+
 	"airdb.io/airdb/sailor"
 	"github.com/airdb/scf-airdb/model/vo"
 	"github.com/pkg/errors"
 )
 
+type Response struct {
+	Success bool               `json:"success"`
+	Error   interface{}        `json:"error"`
+	Data json.RawMessage `json:"data"`
+}
+
 func ListHostedZone(input *vo.ListHostedZoneReq,
 ) (*vo.ListHostedZoneResp, error) {
-	var output vo.ListHostedZoneResp
+	output :=Response{}
 
-	endpoint := "https://scf.baobeihuijia.com" + "/test/airdb/v1/hosted_zone/list"
+	client := sailor.NewHTTPClient()
 
-	var req sailor.HTTPClient
+	client.SetDomain("scf.baobeihuijia.com")
+	client.SetEndpoint( "/test/airdb/v1/hosted_zone/list")
+	client.SetBody(&input)
+	client.SetUserAgent("scf-airdb/v0.0.1")
 
-	req.SetURL(endpoint)
-	req.SetBody(&input)
-
-	if err := sailor.HTTPRequest(&req, &output); err != nil {
+	if err := client.HTTPRequest(client, &output); err != nil {
+		fmt.Println(err)
 		return nil, errors.Wrap(err, "request failed")
 	}
-	return &output, nil
+
+	res := vo.ListHostedZoneResp{}
+	err := json.Unmarshal(output.Data, &res)
+	if err != nil {
+		return nil, errors.Wrap(err, "unmarshal fail")
+	}
+
+	return &res, nil
 }
